3c158bd09ace9b9651e2e387df07a3a30cc977a4
Cody Pope <cody@codys-mac-mini.lan>
2022-02-04 21:02:32 -0500

    moving import inside declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
-import * as AWS from "aws-sdk";
-
 declare module "serverless-dynamodb-client" {
+  import * as AWS from "aws-sdk";
   export const doc: AWS.DynamoDB.DocumentClient;
   export const raw: AWS.DynamoDB;
 }
